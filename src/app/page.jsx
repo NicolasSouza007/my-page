55e1d3c8efb3c18d6253255505dbe2c968f89539
Nicolas Santos <124842027+NicolasSouza007@users.noreply.github.com>
2025-02-24 17:07:22 -0300

    Primeiro projeto next.js

---
+++ src/app/page.jsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div>
+            {/* Conteúdo da página */}
+            <h2>Minha Página</h2>
+        </div>
+    );
+}
---
+++ src/components/Header/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='bg-[#06265D]'>
+            <div className="flex justify-center px-6 py-6 max-w-screen-xl m-auto">
+                <h1 className='text-white'>Header de Teste</h1>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
