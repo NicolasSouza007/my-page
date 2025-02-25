0366cde9e4cde46f08735fdb395c5ea933b9d066
Nicolas Santos <124842027+NicolasSouza007@users.noreply.github.com>
2025-02-25 14:59:07 -0300

    ajuste de Banner

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,14 @@
 import "@/style/global.css";
 import Header from "@/components/Header/header";
-// import Banner from "@/components/Banner/Banner2";
-
+import Banner from "@/components/Banner/banner2";
 
 export default function RootLayout({ children }) {
     return (
         <html lang="pt-BR">
             <body>
                 <Header />  {/* Mantém aqui se for um Server Component */}
-                {children}
-
-                {/* <Banner /> */}
-
+                {children}  {/* Exibe o conteúdo dinâmico aqui */}
+                <Banner />  Mantém aqui se for um Server Component
             </body>
         </html>
     );
---
+++ src/components/Banner/banner2.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+    const Banner = () => {
+    console.log("Banner renderizado");
+    return (
+        <div className="bg-blue-500 p-4 text-white">
+            <h1>Bem-vindo ao Banner!</h1>
+        </div>
+    );
+}
+
+export default Banner;
