a317c893a5de5f32746e8c573162aec6780de834
Nicolas <nicolsebrian007@gmail.com>
2025-02-24 21:20:22 -0300

    header feito

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,10 +1,20 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Header = () => {
     return (
-        <header className='bg-[#06265D]'>
-            <div className="flex justify-center px-6 py-6 max-w-screen-xl m-auto">
-                <h1 className='text-white'>Header de Teste</h1>
+        <header className="bg-[#06265D] w-full">
+            <div className="flex justify-between items-center py-4 max-w-screen-xl w-full mx-auto px-0">
+                {/* Logo no canto esquerdo */}
+                <div className="w-20 ml-0"> 
+                    <Image src="/Logo1.png" width={80} height={50} alt="Logo" priority className="w-full h-auto" />
+                </div>
+
+                {/* Menu no canto direito */}
+                <nav className="flex gap-6 text-white text-sm mr-6">
+                    <a href="#" className="hover:text-gray-300">Home</a>
+                    <a href="#" className="hover:text-gray-300">Tópicos</a>
+                </nav>
             </div>
         </header>
     );
