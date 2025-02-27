3bccd007f494a80d40e9e108ce047d3d8a68da35
Nicolas Santos <124842027+NicolasSouza007@users.noreply.github.com>
2025-02-27 14:18:49 -0300

    tento ajustar o footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,17 +5,20 @@ import Footer from "@/components/Footer/footer";
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="pt-BR">
-            <body>
-                <Header />  {/* Mantém aqui se for um Server Component */}
-                {children}  {/* Exibe o conteúdo dinâmico aqui */}
-                 {/* Mantém aqui se for um Server Component */}
-                 <div className="min-h-screen flex flex-col">
-            <main className="flex-grow"></main>
-            <Footer />
-        </div>   
-            </body>
-            
-        </html>
+        <div className="flex flex-col min-h-screen">
+         
+                <html lang="pt-BR">
+                    <body>
+                        <Header />  {/* Mantém aqui se for um Server Component */}
+                        {/*  Exibe o conteúdo dinâmico aqui */}
+                        {/* Mantém aqui se for um Server Component */}
+                        <div className="min-h-screen flex flex-col">
+                            <main className="flex-grow">{children}</main>
+                            <Footer />
+                        </div>   
+                    </body>
+                    
+                </html>
+        </div>
     );
 }
