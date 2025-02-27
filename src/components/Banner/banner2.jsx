3d3e6c5b9452d4843c4d23f37d8448f43e119b5f
Nicolas <nicolsebrian007@gmail.com>
2025-02-26 23:22:52 -0300

    Ajuste de footer, adicionado os campos de guias

--- src/components/Banner/banner2.jsx
+++ src/components/Banner/banner2.jsx
@@ -14,7 +14,7 @@ const Banner = () => {
     console.log("Banner renderizado");
 
     return (
-        <div className="bg-[#709ac9] p-4 text-white py-[15px] px-[60px] text-center">
+        <div className="bg-[#709ac9] p-4 text-black py-[15px] px-[60px] text-center">
             <h1 className="text-3xl font-bold">Central de Ajuda</h1>
             <h2 className="text-lg mt-2">Encontre as informações que você precisa.</h2>
 
@@ -22,7 +22,7 @@ const Banner = () => {
             <div className="mt-4 flex justify-center items-center">
                 <input
                     id="search"
-                    className="px-4 py-2 text-black rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 w-64"
+                    className="px-4 py-2 text-black rounded-lg border border-gray-950 focus:outline-none focus:ring-2 focus:ring-blue-400 w-64"
                     type="text"
                     placeholder="Pesquise aqui"
                     value={searchQuery}
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,11 +3,87 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="bg-[#06265D] w-full mt-auto text-white py-4 text-center">
-            <p>© {new Date().getFullYear()} Seu Site. Todos os direitos reservados.</p>
-        </footer>
+      <footer className="bg-[#06265d] text-white text-center p-4 w-full mt-auto">
+        <div className="flex justify-around flex-wrap items-center">
+          {/* Setor Comercial */}
+          <div className="flex flex-col items-center max-w-xs">
+            <p className="mb-3 text-lg font-semibold">SETOR COMERCIAL</p>
+            <a
+              href="https://api.whatsapp.com/send/?phone=11935056614&text&type=phone_number&app_absent=0"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="/img/whats.png"
+                alt="WhatsApp"
+                className="w-[40px] h-[40px] transition-transform duration-200 ease-in hover:scale-110 p-1"
+              />
+            </a>
+          </div>
+  
+          {/* Redes Sociais */}
+          <div className="flex flex-col items-center max-w-xs">
+            <p className="mb-3 text-lg font-semibold">CONHEÇA NOSSAS REDES SOCIAIS</p>
+            <div className="flex gap-4">
+              <a
+                href="https://www.instagram.com/grupopowerinformatica/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="/img/Instagram.png"
+                  alt="Instagram"
+                  className="w-[40px] h-[40px] transition-transform duration-200 ease-in hover:scale-110 p-1"
+                />
+              </a>
+              <a
+                href="https://api.whatsapp.com/send/?phone=1146780655&text&type=phone_number&app_absent=0"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="/img/whats.png"
+                  alt="WhatsApp"
+                  className="w-[40px] h-[40px] transition-transform duration-200 ease-in hover:scale-110 p-1"
+                />
+              </a>
+              <a href="https://pinformatica.com" target="_blank" rel="noopener noreferrer">
+                <img
+                  src="/img/WhatsApp_Image_2025-02-03_at_13.26.47-removebg-preview.png"
+                  alt="Site"
+                  className="w-[40px] h-[40px] transition-transform duration-200 ease-in hover:scale-110 p-1"
+                />
+              </a>
+              <a href="https://www.youtube.com/@pwpsys" target="_blank" rel="noopener noreferrer">
+                <img
+                  src="/img/youtube.png"
+                  alt="YouTube"
+                  className="w-[40px] h-[40px] transition-transform duration-200 ease-in hover:scale-110 p-1"
+                />
+              </a>
+            </div>
+          </div>
+  
+          {/* Setor Financeiro */}
+          <div className="flex flex-col items-center max-w-xs">
+            <p className="mb-3 text-lg font-semibold">SETOR FINANCEIRO</p>
+            <a
+              href="https://api.whatsapp.com/send/?phone=11940234777&text&type=phone_number&app_absent=0"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="/img/whats.png"
+                alt="WhatsApp"
+                className="w-[40px] h-[40px] transition-transform duration-200 ease-in hover:scale-110 p-1"
+              />
+            </a>
+          </div>
+        </div>
+      </footer>
     );
-};
-
-export default Footer;
-
+  };
+  
+  export default Footer;
+  
+  
