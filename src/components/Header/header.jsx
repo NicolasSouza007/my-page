a744d248e0300c5fba8403b94fb2d6b4d42caad4
Nicolas Santos <124842027+NicolasSouza007@users.noreply.github.com>
2025-03-10 15:16:55 -0300

    adicionando o link no header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,19 +1,29 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const Header = () => {
     return (
-        <header className="bg-[#06265D] w-full ">
-            <div className="flex justify-between items-center py-4 max-w-screen-xl w-full mx-auto px-0 ">
-                {/* Logo no canto esquerdo */}
-                <div className="w-20 ml-0"> 
-                    <Image src="/Logo1.png" width={80} height={50} alt="Logo" priority className="w-full h-auto ml- " />
+        <header className="bg-[#06265D] w-full">
+            <div className="flex justify-between items-center py-4 max-w-screen-xl mx-auto px-4">
+                {/* Logo no canto esquerdo com link para a página inicial */}
+                <div className="w-20">
+                    <Link href="/">
+                        <Image 
+                            src="/Logo1.png" 
+                            width={80} 
+                            height={50} 
+                            alt="Logo" 
+                            priority 
+                            className="w-full h-auto cursor-pointer"
+                        />
+                    </Link>
                 </div>
 
                 {/* Menu no canto direito */}
                 <nav className="flex gap-6 text-white text-sm mr-6">
-                    <a href="#" className="hover:text-gray-300">Home</a>
-                    <a href="#" className="hover:text-gray-300">Tópicos</a>
+                    <Link href="/" className="hover:text-gray-300">Home</Link>
+                    <Link href="/topics" className="hover:text-gray-300">Tópicos</Link>
                 </nav>
             </div>
         </header>
