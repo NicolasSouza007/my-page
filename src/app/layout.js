60ab5e38a7b9f3c7844aefaef00d5b867c06c348
Nicolas <nicolsebrian007@gmail.com>
2025-04-16 14:03:06 -0300

    config certa

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
-import "@/style/global.css";
-import Header from "@/components/Header/header";
-import Footer from "@/components/Footer/footer";
+import "../style/global.css";
+import Header from "../components/Header/header";
+import Footer from "../components/Footer/Footer";
 
 
 export default function RootLayout({ children }) {
