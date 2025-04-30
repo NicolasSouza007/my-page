d2899125d5a24f832ea3d4b702f7a7ed28190349
Nicolas <nicolsebrian007@gmail.com>
2025-04-30 17:40:51 -0300

    cadastro

--- src/app/Cadastro_Produtos/page.jsx
+++ src/app/Cadastro_Produtos/page.jsx
@@ -1,7 +1,18 @@
 export default function Cadastro_Produtos() {
     return(
        <div>
-        <h1 className=""> Como Realizar Cadastro de Produtos</h1>
+        <h1 className="m-6 text-3xl"> Como Realizar Cadastro de Produtos</h1>
+        <iframe
+            className="m-5"
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/xC3u4wuDvpg"
+            
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+
+            <p>1° Para realizar o cadastro de produtos basta clicar em cadastro e logo em seguida clicar em produtos</p>
        </div>
     )
 }
